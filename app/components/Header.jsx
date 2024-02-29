3771418a27261f2a8e15538b6fa15ede408ede97
krystyna-lyn <67439674+krystyna-lyn@users.noreply.github.com>
2024-02-29 10:46:13 +0000

    add singin, singup pages

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,28 +1,40 @@
+import { auth } from '@clerk/nextjs'
 import Link from 'next/link'
 import React from 'react'
 
 const Header = () => {
-  return (
-    <>
-    <div className="nav bg-gray-700 py-4 px-6 flex items-center justify-between mb-6">
-        <div className="flex items-center">
-            <Link href='/'>
-            <div className="text-lgg uppercase font-bold text-white">
-                Auth Clerk app 
-            </div> 
-            </Link>
-        </div>
-        <div className='text-white'>
-        <Link href='/sign-in' className='text-gray-300 hover:text-white mr-4'>
-            Sign in
-        </Link>
-        <Link href='/sign-un' className='text-gray-300 hover:text-white mr-4'>
-            Sign up
-        </Link>
-        </div>
-    </div>
-    </>
-  )
+
+    const { userId } = auth()
+    console.log(userId)
+
+    return (
+        <>
+            <div className="nav bg-gray-700 py-4 px-6 flex items-center justify-between mb-6">
+
+                <div className="flex items-center">
+                    <Link href='/'>
+                        <div className="text-lgg uppercase font-bold text-white">
+                            Auth Clerk app
+                        </div>
+                    </Link>
+                </div>
+                <div className='text-white flex items-center'>
+                    {!userId && (
+                        <>
+                            <Link href='/sign-in' className='text-gray-300 hover:text-white mr-4'>
+                                Sign in
+                            </Link>
+                            <Link href='/sign-up' className='text-gray-300 hover:text-white mr-4'>
+                                Sign up
+                            </Link>
+                        </>
+                    )
+                    }
+
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Header
