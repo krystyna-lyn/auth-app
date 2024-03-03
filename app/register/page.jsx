1f45f1dc76302ab127554e9db5d261648ec3da46
krystyna-lyn <67439674+krystyna-lyn@users.noreply.github.com>
2024-03-03 10:36:39 +0000

    Update page.jsx

--- app/register/page.jsx
+++ app/register/page.jsx
@@ -1,25 +1,25 @@
- "use client"
+"use client";
 import { useState } from "react";
 import { useSignUp } from "@clerk/nextjs";
 import { useRouter } from "next/navigation";
- 
+
 export default function SignUpForm() {
   const { isLoaded, signUp, setActive } = useSignUp();
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [pendingVerification, setPendingVerification] = useState(false);
   const [code, setCode] = useState("");
   const router = useRouter();
- 
+
   // This function will handle the user submitting their email and password
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (!isLoaded) {
       return;
     }
- 
+
     // Start the sign-up process using the email and password provided
     try {
       await signUp.create({
@@ -28,148 +28,148 @@ export default function SignUpForm() {
         email_address: email,
         password,
       });
- 
+
       // Send the user an email with the verification code
       await signUp.prepareEmailAddressVerification({ strategy: "email_code" });
- 
+
       // Set 'verifying' true to display second form and capture the OTP code
       setPendingVerification(true);
-    } catch (err) {      
+    } catch (err) {
       // This can return an array of errors.
       // See https://clerk.com/docs/custom-flows/error-handling to learn about error handling
       console.error(JSON.stringify(err, null, 2));
     }
   };
- 
+
   // This function will handle the user submitting a code for verification
   const onPressVerify = async (e) => {
     e.preventDefault();
     if (!isLoaded) {
       return;
     }
- 
+
     try {
       // Submit the code that the user provides to attempt verification
       const completeSignUp = await signUp.attemptEmailAddressVerification({
         code,
       });
- 
+
       if (completeSignUp.status !== "complete") {
         // The status can also be `abandoned` or `missing_requirements`
         // Please see https://clerk.com/docs/references/react/use-sign-up#result-status for  more information
         console.log(JSON.stringify(completeSignUp, null, 2));
       }
- 
+
       // Check the status to see if it is complete
       // If complete, the user has been created -- set the session active
       if (completeSignUp.status === "complete") {
-        await setActive({ session: completeSignUp.createdSessionId })
+        await setActive({ session: completeSignUp.createdSessionId });
         // Redirect the user to a post sign-up route
         router.push("/");
       }
-    } catch (err) {      
+    } catch (err) {
       // This can return an array of errors.
       // See https://clerk.com/docs/custom-flows/error-handling to learn about error handling
       console.error(JSON.stringify(err, null, 2));
     }
   };
- 
+
   return (
-    <div className='border p-5 rounded' style={{ width: '500px' }}>
-      <h1 className='text-2xl mb-4'>Register</h1>
+    <div className="border p-5 rounded" style={{ width: "500px" }}>
+      <h1 className="text-2xl mb-4">Register</h1>
       {!pendingVerification && (
-       <form onSubmit={handleSubmit} className='space-y-4 md:space-y-6'>
-       <div>
-         <label
-           htmlFor='first_name'
-           className='block mb-2 text-sm font-medium text-gray-900'
-         >
-           First Name
-         </label>
-         <input
-           type='text'
-           name='first_name'
-           id='first_name'
-           onChange={(e) => setFirstName(e.target.value)}
-           className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5'
-           required={true}
-         />
-       </div>
-       <div>
-         <label
-           htmlFor='last_name'
-           className='block mb-2 text-sm font-medium text-gray-900'
-         >
-           Last Name
-         </label>
-         <input
-           type='text'
-           name='last_name'
-           id='last_name'
-           onChange={(e) => setLastName(e.target.value)}
-           className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5'
-           required={true}
-         />
-       </div>
-       <div>
-         <label
-           htmlFor='email'
-           className='block mb-2 text-sm font-medium text-gray-900'
-         >
-           Email Address
-         </label>
-         <input
-           type='email'
-           name='email'
-           id='email'
-           onChange={(e) => setEmail(e.target.value)}
-           className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5'
-           placeholder='name@company.com'
-           required={true}
-         />
-       </div>
-       <div>
-         <label
-           htmlFor='password'
-           className='block mb-2 text-sm font-medium text-gray-900'
-         >
-           Password
-         </label>
-         <input
-           type='password'
-           name='password'
-           id='password'
-           onChange={(e) => setPassword(e.target.value)}
-           className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5'
-           required={true}
-         />
-       </div>
-       <button
-         type='submit'
-         className='w-full text-white bg-blue-600 hover:bg-blue-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center'
-       >
-         Create an account
-       </button>
-     </form>
+        <form onSubmit={handleSubmit} className="space-y-4 md:space-y-6">
+          <div>
+            <label
+              htmlFor="first_name"
+              className="block mb-2 text-sm font-medium text-gray-900"
+            >
+              First Name
+            </label>
+            <input
+              type="text"
+              name="first_name"
+              id="first_name"
+              onChange={(e) => setFirstName(e.target.value)}
+              className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5"
+              required={true}
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="last_name"
+              className="block mb-2 text-sm font-medium text-gray-900"
+            >
+              Last Name
+            </label>
+            <input
+              type="text"
+              name="last_name"
+              id="last_name"
+              onChange={(e) => setLastName(e.target.value)}
+              className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5"
+              required={true}
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="email"
+              className="block mb-2 text-sm font-medium text-gray-900"
+            >
+              Email Address
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              onChange={(e) => setEmail(e.target.value)}
+              className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5"
+              placeholder="name@company.com"
+              required={true}
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="password"
+              className="block mb-2 text-sm font-medium text-gray-900"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              onChange={(e) => setPassword(e.target.value)}
+              className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5"
+              required={true}
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full text-white bg-blue-600 hover:bg-blue-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+          >
+            Create an account
+          </button>
+        </form>
       )}
       {pendingVerification && (
-         <div>
-         <form className='space-y-4 md:space-y-6'>
-           <input
-             value={code}
-             className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5'
-             placeholder='Enter Verification Code...'
-             onChange={(e) => setCode(e.target.value)}
-           />
-           <button
-             type='submit'
-             onClick={onPressVerify}
-             className='w-full text-white bg-blue-600 hover:bg-blue-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center'
-           >
-             Verify Email
-           </button>
-         </form>
-       </div>
+        <div>
+          <form className="space-y-4 md:space-y-6">
+            <input
+              value={code}
+              className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5"
+              placeholder="Enter Verification Code..."
+              onChange={(e) => setCode(e.target.value)}
+            />
+            <button
+              type="submit"
+              onClick={onPressVerify}
+              className="w-full text-white bg-blue-600 hover:bg-blue-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+            >
+              Verify Email
+            </button>
+          </form>
+        </div>
       )}
     </div>
   );
